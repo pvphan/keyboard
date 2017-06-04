1932d56503157c8f45190902f3683ef9772fbce1
Paul Vinh Phan <pvphan90@gmail.com>
2017-06-04 21:49:53 +0000

    sending A every 1 sec

--- ble60_main/main.c
+++ ble60_main/main.c
@@ -10,7 +10,7 @@
 #define LED_OFF		(PORTD &= ~(1<<6))
 #define LED_CONFIG	(DDRD |= (1<<6))
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
-#define BAUD_RATE 38400
+#define BAUD_RATE 38452
 
 
 void TURN_LED_ON(uint8_t n) {
@@ -113,163 +113,32 @@ void blink_fast(uint8_t k) {
 }
 
 
+const uint8_t byte_array_A[8]       = {0x02, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00};
+const uint8_t byte_array_release[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
 int main(void)
 {
     uint8_t c;
+    int i;
 
     LED_OFF;
     CPU_PRESCALE(0);  // run at 16 MHz
     uart_init(BAUD_RATE);
 
-    // wait for BLE `system_boot` event
-    if (wait_for_ble_wake() != 0) {
-        blink_fast(10);
-        return 0;
-    }
-
-    // DO ALL CONFIGURATION HERE PRIOR TO PAIRING
-    // set bondable mode
-    if (set_bondable_mode() != 0) {
-        blink_fast(10);
-        return 0;
-    }
-
-    // set the GAP mode to discoverable
-    if (set_gap_mode() != 0) {
-        blink_fast(10);
-        return 0;
-    }
-
-    // set security manager params
-    if (set_sm_params() != 0) {
-        blink_fast(10);
-        return 0;
-    }
-
-    
-    uint8_t *handle = (uint8_t*) malloc(sizeof(uint8_t));
-    uint8_t *payload = (uint8_t*) malloc(16*(sizeof(uint8_t)));
-
-
-    // WAIT FOR PAIRING ===========================================
-    // get Attribute Database Status Event ==> "notifications are enabled"
-    if (get_attribute_database_status() != 0) {
-        blink_fast(10);
-        return 0;
-    }
-    // WE SHOULD BE PAIRED NOW! ===================================
-
-    // get Connection Status Event
-    if (get_connection_status(handle, payload) != 0) {
-        blink_fast(10);
-        return 0;
-    }
-
-
-    //blink_slow(5);
-    //// blink the payload
-    //uint8_t i;
-    //for (i=0; i<16; i++) {
-    //    write_to_leds(payload+i);
-    //}
-    //blink_slow(5);
-
-    
-    // TODO: this doesnt work yet, gets error
-    // start encryption
-    uint8_t *result = (uint8_t*) malloc(2*(sizeof(uint8_t)));
-
-    int encrypt_rc = start_encryption(handle, &result);
-
-    if (encrypt_rc == 0) {
-        // we're good!
-        night_rider_leds();
-        //TURN_LED_ON(0);
-        //_delay_ms(2000);
-        //TURN_LED_OFF(0);
-    } else {
-        if (encrypt_rc == 1) {
-            // not quite right, write the error code to LEDs
-            blink_fast(8);
-            write_to_leds(*result);
-            write_to_leds(*(result+1));
-            blink_fast(8);
-        } else if (encrypt_rc == -1) {
-            // this is bad
-            blink_slow(5);
-        } else {
-            // this is really bad
-            blink_slow(10);
-        }
-    }
-
+    LED_ON;
 
     while (1) {
 
-        while (uart_available()) {
-            c = uart_getchar();
-            write_to_leds(c);
+        uart_putchar(0xFF);
+        for (i=0; i<8; i++) {
+            uart_putchar(byte_array_A[i]);
         }
-        _delay_ms(25);
 
-
-        //    if (can_bond == 1 && has_sm_params_set == 1) {
-
-        //        // WAIT FOR BONDING REQUEST
-        //        
-        //        // try to send keystrokes
-        //        
-        //        // send 'press A' payload
-        //        for (i=0; i<8; i++) {
-        //            c = ble_keystroke_header[i];
-        //            uart_putchar(c);
-        //        }
-        //        for (i=0; i<8; i++) {
-        //            c = hid_press_A[i];
-        //            uart_putchar(c);
-        //        }
-
-        //        // get response
-        //        for (i=0; i<6; i++) {
-        //            c = uart_getchar();
-
-        //            if (c == host_received_A_response[i]) {
-        //                host_received_A_response_score++;
-        //            } else {
-        //                if (i == 4) {
-        //                    error_code[0] = c;
-        //                } else if (i == 5) {
-        //                    error_code[1] = c;
-        //                }
-        //            }
-        //        }
-
-        //        // blink back the error code: 0x03, 0x04 ==> 0x0403 (Write not permitted)
-        //        if (host_received_A_response_score == 6) {
-        //            got_A = 1;
-        //        } else {
-        //            for (i=0; i<2; i++) {
-        //                blink_slow(error_code[i]);
-        //                _delay_ms(1000);
-        //            }
-        //        }
-
-        //        if (got_A == 1) {
-        //            LED_ON;
-        //        }
-
-        //        // send 'release A' payload
-        //        for (i=0; i<8; i++) {
-        //            c = ble_keystroke_header[i];
-        //            uart_putchar(c);
-        //        }
-        //        for (i=0; i<8; i++) {
-        //            c = hid_release_A[i];
-        //            uart_putchar(c);
-        //        }
-
-        //        _delay_ms(5000);
-        //    }
+        //uart_putchar(0xFF);
+        //for (i=0; i<8; i++) {
+        //    uart_putchar(byte_array_release[i]);
         //}
+
+        _delay_ms(1000);
     }
 }
